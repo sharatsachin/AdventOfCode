df6ffa0420ea7a9b6d3a1a487ee50c99c1e00fa0
Sharat Sachin <sharat.ss.sachin@gmail.com>
2021-12-04 10:15:25 +0000

    Initial commit

---
+++ day4/main.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int P = 100;  // participants
+
+int main() {
+  string str;
+  cin >> str;
+
+  vector<int> v;
+  stringstream ss(str);
+  while (ss.good()) {
+    string substr;
+    getline(ss, substr, ',');
+    v.push_back(stoi(substr));
+  }
+
+  vector<map<int, pair<int, int>>> vmp(P);
+
+  for (int i = 0; i < P; i++) {
+    int a;
+    for (int j = 0; j < 5; j++) {
+      for (int k = 0; k < 5; k++) {
+        cin >> a;
+        vmp[i][a] = {j, k};
+      }
+    }
+  }
+
+  vector<vector<vector<int>>> marked(P, vector<vector<int>>(2, vector<int>(5, 0)));
+  vector<vector<vector<int>>> done(P, vector<vector<int>>(5, vector<int>(5, 0)));
+  vector<int> finished(P, 0);
+
+  int winner = -1;
+  for (auto el : v) {
+    for (int i = 0; i < P; i++) {
+      if (finished[i] == 1) continue;
+
+      if (vmp[i].count(el)) {
+        marked[i][0][vmp[i][el].first]++;
+        marked[i][1][vmp[i][el].second]++;
+        done[i][vmp[i][el].first][vmp[i][el].second] = 1;
+
+        if (marked[i][0][vmp[i][el].first] == 5 || marked[i][1][vmp[i][el].second] == 5) {
+          finished[i] = 1;
+          int sumf = accumulate(finished.begin(), finished.end(), 0);
+
+          int sum = 0;
+          for (auto [x, y] : vmp[i]) {
+            if (done[i][y.first][y.second] == 0) sum += x;
+          }
+          if (sumf == 1 || sumf == P) {
+            cout << "Position " << sumf << " has points " << el * sum << '\n';
+            if (sumf == 100) return 0;
+          }
+        }
+      }
+    }
+  }
+  return 0;
+}
