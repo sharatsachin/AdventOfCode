d0d22d9b26865bc33276416209e18cd8a959d0ad
Sharat Sachin <sharat.ss.sachin@gmail.com>
2021-12-06 11:09:52 +0000

    updated day 6 code

--- day6/main.cpp
+++ day6/main.cpp
@@ -4,28 +4,6 @@ using namespace std;
 int main(int argc, char** argv) {
     const int days = (argv[1][0] == 'b' ? 256 : 80);
 
-    vector<long long> dp(6, 0);
-
-    // precomputing number of laterns after specified days for [1,5]
-    for (int x = 1; x < 6; x++) {
-        vector<long long> mp(10, 0);
-        mp[x] = 1;
-
-        int curd = 0;
-        // for every day
-        while (curd < days) {
-            // decremnent all the numbers by one
-            int ze = mp[0];
-            for (int i = 0; i < 9; i++) mp[i] = mp[i + 1];
-            // remove old zeros and replaces with equal number of sixes and eights
-            mp[6] += ze, mp[8] += ze;
-            // increment current day counter
-            curd++;
-        }
-        // solution for x = sum of number of all laterns at different stages
-        dp[x] = accumulate(mp.begin(), mp.end(), 0ll);
-    }
-
     // getting input and storing array in vector v
     string str;
     cin >> str;
@@ -37,11 +15,22 @@ int main(int argc, char** argv) {
         v.push_back(stoi(substr));
     }
 
-    // adding up precomputed number of laterns after specified days
-    long long sol = 0;
-    for (auto x : v) {
-        sol += dp[x];
+    // starting number of fishes
+    vector<long long> mp(10, 0);
+    for (auto el : v) mp[el]++;
+
+    int curd = 0;
+    // for every day
+    while (curd < days) {
+        // decremnent all the numbers by one
+        long long ze = mp[0];
+        for (int i = 0; i < 9; i++) mp[i] = mp[i + 1];
+        // remove old zeros and replaces with equal number of sixes and eights
+        mp[6] += ze, mp[8] += ze;
+        // increment current day counter
+        curd++;
     }
+    long long sol = accumulate(mp.begin(), mp.end(), 0ll);
     cout << sol << "\n";
     return 0;
 }
